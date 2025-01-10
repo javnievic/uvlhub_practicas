4482f3df271d62b478d90cc9a76f982efa591b53
Javier Nieto <javiernieto03.jnv@gmail.com>
2025-01-10 15:01:12 +0100

    feat: commit 1

--- app/modules/dataset/api.py
+++ app/modules/dataset/api.py
@@ -26,3 +26,4 @@ def init_blueprint_api(api):
     """ Function to register resources with the provided Flask-RESTful Api instance. """
     api.add_resource(DataSetResource, '/api/v1/datasets/', endpoint='datasets')
     api.add_resource(DataSetResource, '/api/v1/datasets/<int:id>', endpoint='dataset')
+# Commit 1
